cd44fc51c592557be9ad4538e669e312932e8044
Marknjo <marknjo7@gmail.com>
2022-03-17 19:19:48 +0300

    Handle project form submission

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,44 @@
 "use strict";
 class ProjectInputs {
     constructor() {
+        this.titleInputValue = '';
+        this.descriptionInputValue = '';
+        this.peopleInputValue = '';
         this.templateEl = document.getElementById('project-input');
         this.appRootEl = document.getElementById('app');
         const importedDomEl = document.importNode(this.templateEl.content, true);
         this.domEl = importedDomEl.firstElementChild;
         this.domEl.id = 'user-input';
+        this.addEffects();
         this.render();
+    }
+    setFormInputsWithValues() {
+        const formData = new FormData(this.domEl);
+        const { title, description, people } = Object.fromEntries(formData);
+        this.titleInputValue = title;
+        this.descriptionInputValue = description;
+        this.peopleInputValue = people;
+    }
+    getFormInputsAndValidate() {
+        this.setFormInputsWithValues();
+        const enteredTitle = this.titleInputValue;
+        const enteredDescription = this.descriptionInputValue;
+        const enteredPeople = +this.peopleInputValue;
+        return [enteredTitle, enteredDescription, enteredPeople];
+    }
+    handleFormSubmit(event) {
+        event.preventDefault();
+        const validatedInputs = this.getFormInputsAndValidate();
+        if (Array.isArray(validatedInputs)) {
+            const [title, description, people] = validatedInputs;
+            console.log({ title, description, people });
+        }
     }
     render() {
         this.appRootEl.insertAdjacentElement('afterbegin', this.domEl);
+    }
+    addEffects() {
+        document.addEventListener('submit', this.handleFormSubmit.bind(this));
     }
 }
 const projects = new ProjectInputs();
--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,47 @@
 
 /// FIXME: IMPLEMENTATION -> remove this line
 
+interface ProjectInputValues {
+  title: string;
+  description: string;
+  people: number;
+}
+
 /**
  * Project input Class -> Responsible for rendering project adding form
  */
 class ProjectInputs {
   /**
-   * @param {HTMLTemplateElement} templateEl Gets the template holding the form element
+   * @property Gets the template holding the form element
    */
   templateEl: HTMLTemplateElement;
 
   /**
-   * @param {HTMLDivElement} appRootEl App root container
+   * @property App root container
    */
   appRootEl: HTMLDivElement;
 
   /**
-   * @param {HTMLFormElement} domEl stores/holds the form element
+   * @property stores/holds the form element
    */
   domEl: HTMLFormElement;
+
+  ///// Handle Form Inputs
+
+  /**
+   * @property Holds the form title input value
+   */
+  titleInputValue: FormDataEntryValue = '';
+
+  /**
+   * @property holds the form description input value
+   */
+  descriptionInputValue: FormDataEntryValue = '';
+
+  /**
+   * @property holds the form people input value
+   */
+  peopleInputValue: FormDataEntryValue = '';
 
   /**
    * Initializes project inputs on login
@@ -41,8 +64,67 @@ class ProjectInputs {
 
     this.domEl.id = 'user-input';
 
+    /// Add elements to the form
+
+    /// Handle events handlers
+    this.addEffects();
+
     /// Render the element on the UI
     this.render();
+  }
+
+  /**
+   * Sets form properties with submitted values -> title, description, people
+   * Utilizes FormData
+   */
+  private setFormInputsWithValues() {
+    const formData = new FormData(this.domEl);
+    const { title, description, people } = Object.fromEntries(formData);
+
+    this.titleInputValue = title;
+    this.descriptionInputValue = description;
+    this.peopleInputValue = people;
+  }
+
+  /**
+   * Gets user input values
+   * @returns {[string, string, number] | void} A tuple of title, description, people or void
+   */
+  private getFormInputsAndValidate(): [string, string, number] | void {
+    // Extract input values
+    /// SetProject input values -> title, description, and people
+    this.setFormInputsWithValues();
+
+    /// Assign values
+    const enteredTitle = this.titleInputValue as string;
+    const enteredDescription = this.descriptionInputValue as string;
+    const enteredPeople = +this.peopleInputValue as number;
+
+    /// TODO: Handle validations -> min, max, minLength, maxLength, required, isNumber
+
+    /// Validation logic -> if no errors send the values
+
+    return [enteredTitle, enteredDescription, enteredPeople];
+  }
+
+  /**
+   * Handles form submission Event
+   * @param {Event} event Event handler
+   */
+  private handleFormSubmit(event: Event) {
+    // Prevent default
+    event.preventDefault();
+
+    // get the form input values: Event listener submit
+    const validatedInputs = this.getFormInputsAndValidate();
+
+    // Put the content to the store -> Requires a store to handle form submit
+    if (Array.isArray(validatedInputs)) {
+      const [title, description, people] = validatedInputs;
+
+      /// TODO: Send project item to the store
+      console.log({ title, description, people });
+    }
   }
 
   /**
@@ -50,6 +132,16 @@ class ProjectInputs {
    */
   private render() {
     this.appRootEl.insertAdjacentElement('afterbegin', this.domEl);
+  }
+
+  /**
+   *  Add events listeners to the dom El
+   *  // TODO: Enforce it during extending the class
+   */
+  private addEffects() {
+    /// Event listener handling form submission
+    // TODO: Implement @Autobind decorator
+    document.addEventListener('submit', this.handleFormSubmit.bind(this));
   }
 }
 
